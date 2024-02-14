ffd9e676fdfd70fb3717b9cf0c636e67679f5459
Rebeca Gasso <Rebeca_Gasso@gap.com>
2024-02-13 22:28:36 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Rechart from "./components/Rechart";
+import {Grid} from "@mui/material";
+import Navbar from "./components/Navbar";
+import RechartBar from "./components/RechartBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          xs=4
+        </Grid>
+        <Grid item xs={9}>
+          <Grid
+
+          >
+            <Rechart/>
+            <RechartBar/>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
